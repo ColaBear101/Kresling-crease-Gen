d9b3c60cb624ebb5ac43d4e95f5d278a7830e946
Earth <56668833+ColaBear101@users.noreply.github.com>
2026-05-10 14:30:13 +0700

    Refactor: Add js/main.js entry point

---
+++ js/main.js
@@ -0,0 +1,6 @@
+// Main entry point
+document.addEventListener('DOMContentLoaded', () => {
+  // TODO: call all init functions, binding, and initial draw.
+  // This is a starter stub!
+  console.log('Kresling Crease Generator loaded.');
+});
